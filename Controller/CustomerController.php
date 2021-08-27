1f7090883a7332d035d07c8366963b2a9a64f1ac
phanvantri09 <hulk09022000@gmail.com>
2021-08-28 01:14:31 +0700

    update method search customer

--- Controller/CustomerController.php
+++ Controller/CustomerController.php
@@ -12,32 +12,28 @@ class CustomerController extends BaseController{
     public function index(){
         $kc = $this->customerModel->getAll();
         $k = $this->userModel->getAll();
-        return $this->view('page.pageCustomer',['kc'=>$kc,'k'=>$k]);
-    }
-    public function search(){
-        $items = $this->userModel->getAll();
         if(isset($_POST["btnsearch"])){
             $textsearch = $_POST["textsearch"];
             if(empty($_POST["textsearch"])){
                 echo "<script>alert('Hãy nhập dữ liệu tìm kiếm trước khi nhấn nút tìm kiếm')</script>";    
             }
             else{              
-                foreach($items as $key => $item){
+                foreach($kc as $key => $item){
                     if($item[2]==$textsearch || $item[3]==$textsearch){
-                        
-                        $this->view('page.pageCustomer',['stt'=>$item[0],"ten"=>$intem[1],"mail"=>$item[2],
-                        "sdt"=>$item[3],"date"=>$item[4]]);
                         //chuyển view cho màn hình
-                        echo "<script>alert('tìm thành công')</script>";
+                        return $this->view('page.pageCustomer',['kc'=>$kc,'k'=>$k,'textsearch'=>$textsearch]);
                         exit;
                     }
                     else{
                         echo "<script>alert('không có khách hàng có thông tin trên')</script>";
-                        
+                        return $this->view('page.pageCustomer',['kc'=>$kc,'k'=>$k]);
+                        exit;
                     }
                 }
             }
-        }
+          }
+        return $this->view('page.pageCustomer',['kc'=>$kc,'k'=>$k]);
+        
     }
 }
 ?>
--- Views/page/customerView.php
+++ Views/page/customerView.php
@@ -1,19 +1,54 @@
+
 <div class="section">
-<h1>Danh sách thông tin khách hàng</h1>
+
 <form action="" method="POST" class="formsearch">
   <input type="text" name="textsearch" placeholder="Tìm kiếm theo sdt hoặc email">
-  <button type="submit" name="btnsearch"> <i class="fas fa-search"> <a href="index.php?controller=customer&action=search"></a></i> </button>
+  <button type="submit" name="btnsearch"> <i class="fas fa-search"> <a href="index.php?controller=customer&action=index"></a></i> </button>
 </form>
+
+<?php
+  if(isset($textsearch)){
+    ?>
+    <h1>Thông tin khách hàng <?php echo $textsearch; ?> </h1>
+    <table>
+    <tr>
+    <th>STT</th>
+    <th>Họ Tên</th>
+    
+    <th>Email</th>
+    <th>SDT</th>
+    <th>Ngày Sinh</th>
+  </tr>
+    <?php
+    foreach($kc as $key => $item){
+      if($textsearch == $item[3])
+      {
+        ?>
+        <tr>
+    <td><?php echo $item[0]?></td>
+    <td><?php echo $item[1]?></td>
+    <td><?php echo $item[2]?></td>
+    <td><?php echo $item[3]?></td>
+    <td><?php echo $item[4]?></td>
+  </tr>
+      <?php  
+      }
+    } 
+  }
+  ?>
+</table>
+<h1>Danh sách thông tin khách hàng</h1>
 <table>
   <tr>
     <th>STT</th>
     <th>Họ Tên</th>
-    <th>SDT</th>
+    
     <th>Email</th>
+    <th>SDT</th>
     <th>Ngày Sinh</th>
   </tr>
-  <?php 
-  foreach($kc as $key => $item){
+  <?php
+    foreach($kc as $key => $item){
     ?>
   <tr>
     <td><?php echo $item[0]?></td>
@@ -23,7 +58,7 @@
     <td><?php echo $item[4]?></td>
   </tr>
   <?php 
-  }
+      }
   ?>
 </table>
 </div>
